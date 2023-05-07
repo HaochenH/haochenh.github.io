d2fde8848e36cef58dffe87550470a0e01bc660b
Haochen Hu <hhuuucc@Hotmail.com>
2023-05-06 18:07:23 -0700

    Update default.html

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+function googleTranslateElementInit() {
+    var browserLang =
+        window.navigator.language || window.navigator.userLanguage;
+    if (!browserLang.startsWith("en")) {
+        new google.translate.TranslateElement(
+            {
+                pageLanguage: "en",
+                includedLanguages:
+                    "zh-TW,zh-CN,ja,fr,de,ru,es,pt,hi,ms,th,tl,vi,ko",
+                autoDisplay: false,
+            },
+            "google_translate_element"
+        );
+    } else {
+        // Hide the Translate button
+        document.getElementById("google_translate_element").style.display =
+            "none";
+    }
+}
+
+function triggerTranslation() {
+    var translateButton = document.querySelector(".goog-te-combo");
+    if (translateButton) {
+        translateButton.dispatchEvent(new Event("change"));
+    }
+}
+let translated = false;
+window.onload = function () {
+    googleTranslateElementInit();
+    triggerTranslation();
+    setTimeout(function () {
+        triggerTranslation();
+        translated = true;
+    }, 500);
+};
+window.addEventListener("load", function () {
+    if (translated) {
+        return;
+    }
+    googleTranslateElementInit();
+    triggerTranslation();
+    setTimeout(function () {
+        triggerTranslation();
+    }, 500);
+});
